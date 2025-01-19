0024259e09134765368da28a62978230ec500c52
yanicorn3000 <yanadziabiola@gmail.com>
2025-01-19 21:06:11 +0100

    Tips upload

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -2,30 +2,23 @@ import { FC } from "react";
 import Button from "../button/Button";
 import styles from "./Card.module.scss";
 import { Link } from "react-router";
+import { Tip } from "../../utils/tips";
 
-export type TCard = {
-  title: string;
-  img: string;
-  alt: string;
-  description: string;
-  id: number;
-};
-
-const Card: FC<TCard> = ({ title, img, alt, description, id }) => {
+const Card: FC<Tip> = ({ title, img, alt, description, id }) => {
   const href = `/tip/${id}`;
 
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm  bg-white border border-gray-200 rounded-lg shadow flex flex-col">
       <div className="rounded-t-lg overflow-hidden">
         <img className="object-cover h-40 w-full" src={img} alt={alt} />
       </div>
-      <div className="p-5">
+      <div className="flex flex-col p-5 justify-end flex-grow">
         <a href={href}>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {title}
           </h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex-grow">
           {description}
         </p>
         <div className="flex justify-between items-center">
